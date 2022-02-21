79cafbfa003082791573d3d38f06d28f37978afa
Konstantin Zharinov <zharinovkv@gmail.com>
2022-02-21 16:37:55 +0500

    refactor: setEnv function

--- src/env.php
+++ src/env.php
@@ -5,5 +5,5 @@ namespace Vkapi\env;
 function setEnv()
 {
     $dotenv = new \Symfony\Component\Dotenv\Dotenv();
-    $dotenv->load(dirname(__DIR__, 1) . '/.env');
+    $dotenv->load(dirname(__DIR__, 1) . '/.env', dirname(__DIR__, 1) . '/.env.dev');
 }
